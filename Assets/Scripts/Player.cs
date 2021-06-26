29c1d9bb5dca0ef2cbef0b03d99eaf98e42d081a
Jaden Balogh <jadenbalogh@gmail.com>
2021-06-26 16:48:42 -0700

    Add basic move animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,24 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
 
     private Vector2 movement;
+
     private Rigidbody2D rb2D;
+    private Animator animator;
 
     void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector2 moveDir = movement.normalized;
+        if (moveDir.sqrMagnitude > 0)
+        {
+            animator.SetFloat("xDir", moveDir.x);
+            animator.SetFloat("yDir", moveDir.y);
+        }
     }
 
     void FixedUpdate()
